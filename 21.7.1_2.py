26bc75b1200000c94e376ffe7a27b5c5c2753e29
informatik88 <117893981+informatik88@users.noreply.github.com>
2023-02-01 11:08:33 +0300

    Create 21.7.1_2.py

---
+++ 21.7.1_2.py
@@ -0,0 +1,19 @@
+from Cat_class import Cat
+
+#Создаём питомца 1
+
+cat1 = Cat ("Барон", "мальчик", 2)
+
+print("Вид=", cat1.species)
+print("Имя", cat1.name)
+print("Пол", cat1.gender)
+print("Возраст", cat1.age)
+
+#Создаём питомца 2
+
+cat2 = Cat ("Сэм", "мальчик", 2)
+
+print("Вид=", cat2.species)
+print("Имя", cat2.name)
+print("Пол", cat2.gender)
+print("Возраст", cat2.age)
