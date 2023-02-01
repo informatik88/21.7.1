edfdcf18a415824206d48d6d86d520aca3cbaa53
informatik88 <117893981+informatik88@users.noreply.github.com>
2023-02-01 11:07:32 +0300

    Create 21.7.1_1.py

---
+++ 21.7.1_1.py
@@ -0,0 +1,7 @@
+#Cоздаём конструктор класса Cat
+class Cat:
+    def __init__(self, species="кошка", name="", gender="", age=0):
+        self.species = species
+        self.name = name
+        self.gender= gender
+        self.age= age
